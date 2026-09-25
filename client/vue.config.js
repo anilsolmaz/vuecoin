854ffe9fbabccd58c17eaf5df677b3e17ab0e97c
Anil Solmaz <anilsolmaz@users.noreply.github.com>
2026-09-25 00:24:41 -0700

    feat: configure GitHub Pages auto-deploy script and subpath routing

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/',
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || (process.env.NODE_ENV === 'production' ? '/vuecoin/' : '/'),
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
